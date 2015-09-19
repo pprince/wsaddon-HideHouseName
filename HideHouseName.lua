6556ff65a6529a842dfb2345787596abd1fbbc47
Paul Prince <paul@littlebluetech.com>
2015-09-19 08:00:00 -0400

    First attempt.. Not seeing event fire? (Not Functional)

---
+++ HideHouseName.lua
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------------------------------
+--              HideHouseName
+--              Copyright (c) J Teatime.
+-----------------------------------------------------------------------------------------------
+
+require "Window"
+
+local HideHouseName = {}
+
+function HideHouseName:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+
+    return o
+end
+
+function MyAddon:OnDependencyError(strDep, strError)
+    return true
+end
+
+function HideHouseName:Init()
+    local bHasConfigureFunction = false
+    local strConfigureButtonText = ""
+    local tDependencies = {
+        -- "None",
+    }
+    Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
+
+    Apollo.RegisterEventHandler("ObscuredAddonVisible", "onObscuredAddonVisible", self)
+end
+
+function HideHouseName:OnLoad()
+end
+
+function onObscuredAddonVisible(strLoadedAddon)
+    Print("DEBUG #001")
+    if strLoadedAddon == "HousingRemodel" then
+        Print("DEBUG #002")
+        local HousingRemodel = Apollo.GetAddon("HousingRemodel")
+        if HousingRemodel then
+            Print("DEBUG #003")
+            if HousingRemodel.wndConfigure then
+                Print("DEBUG #004")
+                HousingRemodel.wndConfigure:SetSprite(nil)
+                Print("DEBUG #005")
+            end
+        end
+    end
+end
+
+-- HideHouseName Instance
+local HideHouseNameInst = HideHouseName:new()
+HideHouseNameInst:Init()
